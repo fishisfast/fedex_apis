cd804a403ba188c8509e3aec6eee17107899b371
Lucas Fais <lucasfais@gmail.com>
2023-05-11 16:51:59 -0300

    Add excon and vcr

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,10 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "fedex_apis"
 
 require "minitest/autorun"
+
+require "vcr"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "test/vcr_cassettes"
+  config.hook_into :excon
+end
